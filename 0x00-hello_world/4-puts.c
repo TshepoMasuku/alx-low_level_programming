91e8140e853dffd773bfd69e3b0af64545b5a88a
TshepoMasuku <tshepomasuku16@gmail.com>
2023-03-20 19:21:23 +0200

    4-puts.c try00

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *This programme prints out a string
+ *
+ *return: always 0 (correct)
+ */
+
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle");
+	return (0);
+}
