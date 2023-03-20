2be3a66d3ac3e9b9a794d86af9ad5774f71bff51
TshepoMasuku <tshepomasuku16@gmail.com>
2023-03-20 14:30:14 +0200

    0-preprocessor try02

---
+++ 0x00-hello_world/hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'this programme prints out HELLO WORLD!'
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	printf("Hello, World!");
+	return (0);
+}
