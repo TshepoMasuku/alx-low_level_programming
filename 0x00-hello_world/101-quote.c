09c5e7ede9689ce0f496891bb4269fa34b1e2145
TshepoMasuku <tshepomasuku16@gmail.com>
2023-03-21 03:50:18 +0200

    6-size.c try06

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - This programme prints out a string without printf() & puts().
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
