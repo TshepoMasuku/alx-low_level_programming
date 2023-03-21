dca7b40014cb9a78b14f9cdb12bda155f73fd590
TshepoMasuku <tshepomasuku16@gmail.com>
2023-03-21 02:23:21 +0200

    101-quote.c try00

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - This programme prints out a string without printf() & puts().
+ *
+ *Return: Always 1 (Success)
+ *
+ */
+
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
