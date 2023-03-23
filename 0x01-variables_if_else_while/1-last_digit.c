27418f06992b3fe41e18ef914ea0e8df814d8e54
TshepoMasuku <tshepomasuku16@gmail.com>
2023-03-23 13:29:46 +0200

    1-last_digit try01

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 		printf("Last digit of %i is %i and is greater than 5\n",
 		n, last_digit);
 	}
-	if (last_digit <= 5)
+	if (last_digit <= 5 && last_digit != 0)
 	{
 		printf("Last digit of %i is %i and is less than 6 and not 0\n",
 		n, last_digit);
