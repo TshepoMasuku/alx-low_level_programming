242b660da1dfab4cd4fc646f219659eb52de1408
TshepoMasuku <tshepomasuku16@gmail.com>
2023-03-23 20:09:14 +0200

    6-print_numberz try00

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- The entry point
+ *
+ * Return: 0 Always (Success)
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
