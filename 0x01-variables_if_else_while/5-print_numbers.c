f25635907242e710fe9304b33c25662010151434
TshepoMasuku <tshepomasuku16@gmail.com>
2023-03-23 19:58:17 +0200

    5-print_numbers try00

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- The entry point
+ *
+ * Return: 0 Always (Success)
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
