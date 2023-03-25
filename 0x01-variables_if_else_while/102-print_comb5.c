1938990f3c4cb4baebf70b68adc16096551924ab
TshepoMasuku <tshepomasuku16@gmail.com>
2023-03-25 02:42:22 +0200

    102-print_comb5.c try00

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main- Entry Point
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	int i, k, j, z;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (z = 0; z <= 9; z++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(z + '0');
+					if (i != 9 || j != 9 || k != 9 || z != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
