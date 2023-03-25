2f679cf57f5107ca6b7b1ef0c4cf40ffa3ca105d
TshepoMasuku <tshepomasuku16@gmail.com>
2023-03-25 03:42:40 +0200

    102-print_comb5.c try01

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,15 +18,18 @@ int main(void)
 			{
 				for (z = 0; z <= 9; z++)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(z + '0');
-					if (i != 9 || j != 9 || k != 9 || z != 9)
+					if (i < k || j < z)
 					{
-						putchar(',');
+						putchar(i + '0');
+						putchar(j + '0');
 						putchar(' ');
+						putchar(k + '0');
+						putchar(z + '0');
+						if (i != 9 || j != 8 || k != 9 || z != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
 			}
