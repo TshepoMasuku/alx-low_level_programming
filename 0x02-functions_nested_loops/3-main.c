cea7ca32d3901b7ce50713eb191b8ae328df9c57
TshepoMasuku <tshepomasuku16@gmail.com>
2023-04-03 18:31:42 +0200

    3-islower try99

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code.
@@ -9,14 +8,14 @@
 
 int main(void)
 {
-    int r;
+	int r;
 
-    r = _islower('H');
-    putchar(r + '0');
-    r = _islower('o');
-    putchar(r + '0');
-    r = _islower(108);
-    putchar(r + '0');
-    putchar('\n');
-    return (0);
+	r = _islower('H');
+	putchar(r + '0');
+	r = _islower('o');
+	putchar(r + '0');
+	r = _islower(108);
+	putchar(r + '0');
+	putchar('\n');
+	return (0);
 }
